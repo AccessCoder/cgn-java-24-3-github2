816145993a8763c63edafa5c125e9ff1364dba80
Dominic <andro5@web.de>
2024-06-10 10:01:59 +0200

    add bugfix

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I´m a BUG!");
+        System.out.println("I´m a fixed BUG!");
+        System.out.println("Bug got fixed!");
 
     }
 }
